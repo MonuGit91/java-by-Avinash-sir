6538b891901fc7c6fece58e292e3e0fd28344430
MonuGit91 <monuc20082000@gmail.com>
2021-10-20 14:07:09 +0530

    making simple

--- Students.java
+++ Students.java
@@ -1,24 +1,13 @@
 public class Students
 {
+
 	String name;
 	String collegeName;
 	String email;
 	String branch;
 	String address;
 	int rollNo;
-	long  phoneNo;
 	char gender;
-
-	Students()
-	{
-		name = "Monu Choudhary";
-		collegeName = "BCE";
-		email = "monuc20082000@gmail.com";
-		branch = "CSE";
-		address = "Asansol, 713336";
-		rollNo = 192600123;
-		gender = 'M';
-	}
 
 	public String studentName()
 	{
@@ -27,7 +16,17 @@ public class Students
 	}
 	public static void main(String[] str)
 	{
-		Students student = new Students();
-		System.out.println(student.studentName() + student.name); 
+
+		Students stu = new Students();
+
+		stu.name = "Monu Choudhary";
+        stu.collegeName = "BCE";
+        stu.email = "monuc20082000@gmail.com";
+        stu.branch = "CSE";
+        stu.address = "Asansol, 713336";
+        stu.rollNo = 192600123;
+        stu.gender = 'M';
+	
+		System.out.println(stu.studentName() + stu.name); 
 	}
 }
